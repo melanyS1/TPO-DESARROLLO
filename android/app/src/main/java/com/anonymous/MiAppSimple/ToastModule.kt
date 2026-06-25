4de2daa54339c90bf00008d9e161dc4320b952ca
melany serquen <serquenmelany.b@gmail.com>
2026-06-25 09:06:07 -0300

    Agrego configuracion Android

---
+++ android/app/src/main/java/com/anonymous/MiAppSimple/ToastModule.kt
@@ -0,0 +1,24 @@
+package com.anonymous.MiAppSimple
+
+import android.widget.Toast
+import com.facebook.react.bridge.ReactApplicationContext
+import com.facebook.react.bridge.ReactContextBaseJavaModule
+import com.facebook.react.bridge.ReactMethod
+
+class ToastModule(
+    reactContext: ReactApplicationContext
+) : ReactContextBaseJavaModule(reactContext) {
+
+    override fun getName(): String {
+        return "ToastModule"
+    }
+
+    @ReactMethod
+    fun show(message: String) {
+        Toast.makeText(
+            reactApplicationContext,
+            message,
+            Toast.LENGTH_SHORT
+        ).show()
+    }
+}
